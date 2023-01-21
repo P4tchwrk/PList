6afd641ac4d8044bb582eb0014986fef48f45c23
hugo <hugo.boilloux@ecole2600.com>
2023-01-21 14:09:32 +0100

    Big big plane

--- main.c
+++ main.c
@@ -6,17 +6,39 @@
 
 /*
 TODO :
-- Creer des handles ou trouver un moyen pour get les times
+- Enlever les .exe
 */
+
+BOOL SetPrivilege(HANDLE hToken, LPCTSTR lpszPrivilege, BOOL bEnablePrivilege);
+BOOL ListProcessThreads( DWORD dwOwnerPID, INT searched_process_pid);
+BOOL GeneralProcessInfos(char* searched_exec);
 
 int main(int argc, char *argv[])
 {
+    HANDLE hProcess=GetCurrentProcess();
+    HANDLE hToken;
+
+    if (OpenProcessToken(hProcess, TOKEN_ADJUST_PRIVILEGES, &hToken))
+    {
+        //if(!SetPrivilege(hToken, SE_SECURITY_NAME, TRUE))
+        if(!SetPrivilege(hToken, SE_DEBUG_NAME, TRUE))
+        {
+            printf("Error setting privileges");
+        }
+        else
+            printf("New privileges activated\n");
+        
+        CloseHandle(hToken);
+    }
+    else
+        printf("Error getting process token");
+
     /////////////////Parse args/////////////////
     char *searched_exec = NULL;
     BOOL thread_search = FALSE;
     int searched_thread_id;
     
-    for(int i=1; i< argc; i++) // Evite le nom de fichier
+    for(int i=1; i< argc; i++) // Evite le nom de fichier en entree
     {
         if(argv[i] == "-h")
         {
@@ -37,18 +59,36 @@ int main(int argc, char *argv[])
         }
             
     }
-    if(thread_search)
-        printf("Thread search");
     
-    //////////////////////////////////////////// End parse args
+    //////////////////////////////////////////// End parse args    
+
+    if(thread_search)   // Launch different functions
+    {
+        ListProcessThreads(searched_thread_id, searched_thread_id);
+        //if(ShowThreadInfos(searched_thread_id))
+        //    printf("Error : %d\n", GetLastError());
+        
+        return 0;
+    }
+    else
+    {
+        if(GeneralProcessInfos(searched_exec))
+            printf("Error\n");
+        return 0;
+    }
     
+    return 0;
+}
+
+BOOL GeneralProcessInfos(char* searched_exec)
+{
     /////////////////////Create snapshot
     
     DWORD self_process_id = 0;
     HANDLE hsnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, self_process_id);
     if(hsnapshot == NULL)
     {
-        printf("Erreur de la snapshot");
+        printf("Erreur de la snapshot\n");
         return 1;
     }
 
@@ -63,12 +103,10 @@ int main(int argc, char *argv[])
     }
     if(!Process32First(hsnapshot, &process)) // Get le premier process
     {        
-        printf("Erreur dans la liste de process");
+        printf("Erreur dans la liste de process\n");
         return 1;
     }
-    
-    //////////////////////End create snapshot
-        
+    //////////////////////
     // Formatage
     int max_len = 0;
     do
@@ -76,82 +114,131 @@ int main(int argc, char *argv[])
         if(strlen(process.szExeFile) > max_len)
             max_len = strlen(process.szExeFile);
     }while(Process32Next(hsnapshot, &process));
-    /////////////////////
-    Process32First(hsnapshot, &process);
-    PROCESS_MEMORY_COUNTERS process_memory;
-    HANDLE memory_handle;
-    LPFILETIME start_time;
-    LPFILETIME end_time;
-    LPFILETIME kernel_time;
-    LPFILETIME user_time;
-    
+
     printf("<Exe>");
     for(int i=0; i<max_len/5; i++)
         printf("\t");
-    
-    BOOL tab_inited = FALSE;
-    char *process_name;
+    printf("<Pid>\t<Pri>\t<Thrds>\t<Hnd>\t<Priv>\t\t\t\t<CPU Time>\t\t<Elapsed Time>\n");    // Top of the returned tab
+    /////////////////////
 
-    do
+    Process32First(hsnapshot, &process);
+    HANDLE memory_handle;
+    LPFILETIME start_time = malloc(sizeof(LPFILETIME));
+    LPFILETIME end_time = malloc(sizeof(LPFILETIME));
+    LPFILETIME kernel_time;
+    LPFILETIME user_time;
+    FILETIME raw_time_to_print;
+    SYSTEMTIME time_to_print;
+    HANDLE process_handle;
+    BOOL valid;
+    PDWORD hndCount;
+    PROCESS_MEMORY_COUNTERS memCount;
+    int rep;
+    int j;
+    
+    char *process_name;
+    do                      /// On parcourt les process
     {
-        // Parse arg
-        if(thread_search)
+        process_name = process.szExeFile;
+        if(searched_exec != NULL && strcmp(searched_exec, process_name) || process.th32ProcessID == 0)
+            continue;
+        
+        printf(process.szExeFile); // Exe
+        for(int i=0; i<max_len/5-(strlen(process.szExeFile)/8); i++)    // Pour que tout soit aligné
         {
-            ListProcessThreads(process.th32ProcessID, searched_thread_id);
+            printf("\t");
+        }
+        
+        printf("%u",process.th32ProcessID); // Pid
+        printf("\t%d", process.cntThreads); // Thrs nbr
+        printf("\t%d", process.pcPriClassBase); // Pri
+        ////////////////// Open a handle for more infos
+        process_handle = NULL;
+        process_handle = OpenProcess(  PROCESS_QUERY_INFORMATION |
+                            PROCESS_VM_READ,
+                            FALSE, process.th32ProcessID);
+        if(process_handle == NULL)  // Test error on handle
+        {
+            //printf("\tError : %d", GetLastError());
+            //printf("\tErreur lors de la création du handle");
         }
         else
         {
-            if(!tab_inited)
+        //////////////////////////// Hnd   
+            if(GetProcessHandleCount(process_handle,&hndCount))
+                printf("\t%d", hndCount);
+            else
+                printf("\t0");  // Au cas ou on set a zero
+        ///////////////////////// Priv
+            memCount.cb = sizeof(PROCESS_MEMORY_COUNTERS);
+            if(GetProcessMemoryInfo(process_handle, &memCount, sizeof(PROCESS_MEMORY_COUNTERS)))
             {
-                printf("<Pid>\t<Pri>\t<Thdrs>\t<Dwsize>\n");    // Top of the returned tab
-                tab_inited = TRUE;
+                printf("\t%d",memCount.PagefileUsage);
+                rep = 1;
+                j = 0;
+                while(rep < memCount.PagefileUsage)
+                {
+                    rep = rep*10;
+                    j += 1; // j le nombre de caracs dans la chaine
+                }
+                for(int i=0; i<2-j/8; i++)    // This shit only to align content
+                {
+                    printf("\t");
+                }
             }
-            process_name = process.szExeFile;
-            if(searched_exec != NULL && strcmp(searched_exec, process_name))
-                continue;
+            else
+                printf("\t0");
+        //////////////////////// Process times        
             
-            printf(process.szExeFile); // Exe
-            for(int i=0; i<max_len/5-(strlen(process.szExeFile)/8); i++)    // Pour que tout soit aligné
+            if(!GetProcessTimes(process_handle, &start_time, &end_time, &kernel_time, &user_time) == 0) // if no error
             {
-                printf("\t");
+                if(LocalFileTimeToFileTime(&raw_time_to_print, &kernel_time))
+                    if(FileTimeToSystemTime(&raw_time_to_print, &time_to_print))
+                        printf("\t\t%d:%d:%d.%d", time_to_print.wHour, time_to_print.wMinute, time_to_print.wSecond, time_to_print.wMilliseconds);    // CPU Time
+
+                end_time = end_time-start_time;
+                if(LocalFileTimeToFileTime(&raw_time_to_print, &end_time))
+                    if(FileTimeToSystemTime(&raw_time_to_print, &time_to_print))
+                        printf("\t\t%d:%d:%d.%d", time_to_print.wHour, time_to_print.wMinute, time_to_print.wSecond, time_to_print.wMilliseconds);  // Elapsed Time
             }
-            
-            printf("%u",process.th32ProcessID); // Pid
-            printf("\t%d", process.cntThreads); // Thrs
-            printf("\t%d", process.pcPriClassBase); // Pri
-            printf("\n");
-            // Get thread id
+            CloseHandle(process_handle);
+            //////////////////
         }
+        printf("\n");
+        // Get thread id
     }while(Process32Next(hsnapshot, &process));
-    return 0;
 }
 
 BOOL ListProcessThreads( DWORD dwOwnerPID, INT searched_process_pid) // When option -d is present
 { 
   HANDLE hThreadSnap = INVALID_HANDLE_VALUE; 
   THREADENTRY32 te32; 
- 
   // Take a snapshot of all running threads  
-  hThreadSnap = CreateToolhelp32Snapshot( TH32CS_SNAPTHREAD, 0 ); 
+  hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0); 
   if( hThreadSnap == INVALID_HANDLE_VALUE ) 
-    return( FALSE ); 
- 
+    return(FALSE); 
   // Fill in the size of the structure before using it. 
   te32.dwSize = sizeof(THREADENTRY32); 
- 
   // Retrieve information about the first thread,
   // and exit if unsuccessful
   if( !Thread32First( hThreadSnap, &te32 ) ) 
   {
     printf("Error");
-    CloseHandle( hThreadSnap );          // clean the snapshot object
-    return( FALSE );
+    CloseHandle(hThreadSnap);          // clean the snapshot object
+    return(FALSE);
   }
-
   // Now walk the thread list of the system,
   // and display information about each thread
   // associated with the specified process
   BOOL tab_inited = FALSE;
+  HANDLE hThread;
+  CONTEXT threadContext;
+  LPFILETIME start_time;
+  LPFILETIME end_time;
+  LPFILETIME kernel_time;
+  LPFILETIME user_time;
+  FILETIME raw_time_to_print;
+  SYSTEMTIME time_to_print;
   do 
   { 
     if( te32.th32OwnerProcessID == dwOwnerPID )
@@ -160,24 +247,88 @@ BOOL ListProcessThreads( DWORD dwOwnerPID, INT searched_process_pid) // When opt
         {
             if(!tab_inited)
             {
-                printf("<Tid>\t<Pri>\t<Cswtch>\t<State>\t<User Time>\t<Kernel Time>\t<Elapsed Time>\n");
+                printf("<Tid>\t<Pri>\t\t<Cswtch>\t<State>\t\t<User Time>\t\t<Kernel Time>\t\t<Elapsed Time>\n");
                 tab_inited = TRUE;
             }
             // infos dans te32
-
             printf("%d\t", te32.th32ThreadID);
             printf("%d\t", te32.tpBasePri);
-            printf("%d\t", te32.cntUsage);
-            
-            //if(GetThreadTimes())
+            //printf("%d\t", te32.cntUsage);
+            hThread = OpenThread(THREAD_QUERY_INFORMATION | THREAD_GET_CONTEXT | THREAD_SUSPEND_RESUME | THREAD_DIRECT_IMPERSONATION, FALSE, te32.th32ThreadID);
+            // Le thread doit nous appartenir (d'ou l'impersonate) et etre suspend pour get le context
+            if(hThread == NULL)
+            {
+                printf("Erreur de récupération du handle thread\n");
+            }
+            else
+            {
+                //////////////// Cswtch
+                SuspendThread(hThread);
+                if(GetThreadContext(hThread, &threadContext))
+                    printf("\t%d",threadContext.ContextFlags);
+                else
+                {
+                    printf("Error: %d\n", GetLastError());
+                    printf("Pas de thread context");
+                }
+                ResumeThread(hThread);
+                /////////////
+                ///// State
+                printf("\t\tMISS");
+                //////A remplir
+                /////////////
+                ///// Times
+                if(GetThreadTimes(hThread, &start_time, &end_time, &kernel_time, &user_time)) // if no error
+                {
+                    if(LocalFileTimeToFileTime(&raw_time_to_print, &user_time))
+                        if(FileTimeToSystemTime(&raw_time_to_print, &time_to_print))
+                            printf("\t\t%d:%d:%d.%d", time_to_print.wHour, time_to_print.wMinute, time_to_print.wSecond, time_to_print.wMilliseconds);    // user Time
+                    if(LocalFileTimeToFileTime(&raw_time_to_print, &kernel_time))
+                        if(FileTimeToSystemTime(&raw_time_to_print, &time_to_print))
+                            printf("\t\t%d:%d:%d.%d", time_to_print.wHour, time_to_print.wMinute, time_to_print.wSecond, time_to_print.wMilliseconds);    // kernel Time
+                    
+                    end_time = end_time-start_time; // elapsed here
+                    if(LocalFileTimeToFileTime(&raw_time_to_print, &user_time))
+                        if(FileTimeToSystemTime(&raw_time_to_print, &time_to_print))
+                            printf("\t\t\t%d:%d:%d.%d", time_to_print.wHour, time_to_print.wMinute, time_to_print.wSecond, time_to_print.wMilliseconds);    // elapsed Time
+                    //////////////
+                    CloseHandle(hThread);
+                }
+            }
             printf("\n");
-            printf("PID : %d\n", te32.th32OwnerProcessID);
         }
-        
-
     }
   } while( Thread32Next(hThreadSnap, &te32 ) ); 
 
   CloseHandle( hThreadSnap );
   return( TRUE );
+}
+
+
+BOOL SetPrivilege(HANDLE hToken, LPCTSTR lpszPrivilege, BOOL bEnablePrivilege)
+{
+    LUID luid;
+    BOOL bRet=FALSE;
+
+    if (LookupPrivilegeValue(NULL, lpszPrivilege, &luid))
+    {
+        TOKEN_PRIVILEGES tp;
+
+        tp.PrivilegeCount=1;
+        tp.Privileges[0].Luid=luid;
+        tp.Privileges[0].Attributes=(bEnablePrivilege) ? SE_PRIVILEGE_ENABLED: 0;
+        //
+        //  Enable the privilege or disable all privileges.
+        //
+        DWORD temp_;
+        if (AdjustTokenPrivileges(hToken, FALSE, &tp, NULL, (PTOKEN_PRIVILEGES)NULL, (PDWORD)NULL))
+        {
+            //
+            //  Check to see if you have proper access.
+            //  You may get "ERROR_NOT_ALL_ASSIGNED".
+            //
+            bRet=(GetLastError() == ERROR_SUCCESS);
+        }
+    }
+    return bRet;
 }
